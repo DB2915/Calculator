da002d7cfb5e3c3b71d7671bedd8802096b4601c
DB2915 <db2915@mail.ru>
2018-06-01 14:30:24 +0300

    Простые действия
    Вычисления в одно действие и с =, и с +/-.
    Багов вроде нет.

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -12,11 +12,15 @@ namespace Calculator
 {
     public partial class Form1 : Form
     {
+        int part = 0;
+        int total = 0;
+        char oper = '+';
         public Form1()
         {
             InitializeComponent();
         }
 
+        //Ввод чисел
         private void button1_Click(object sender, EventArgs e)
         {
             label1.Text += "1";
@@ -65,6 +69,96 @@ namespace Calculator
         private void button10_Click(object sender, EventArgs e)
         {
             label1.Text += "0";
+        }
+        //Конец ввода цифр
+        //"/"
+        private void button11_Click(object sender, EventArgs e)
+        {
+            label1.Text += "/";
+        }
+
+        //*
+        private void button12_Click(object sender, EventArgs e)
+        {
+            label1.Text += "*";
+        }
+        //+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (label1.Text.Length>0)
+            {
+                oper = '+';
+                int a = Convert.ToInt32(Convert.ToString(label1.Text[0]));
+                int b = Convert.ToInt32(Convert.ToString(label1.Text[2]));
+                for (int i = 0; i < label1.Text.Length; i++)
+                {
+                    if (label1.Text[i] == '*')
+                    {
+                        part = a * b;
+                    }
+                    else if (label1.Text[i] == '/')
+                    {
+                        part = a / b;
+                    }
+                }
+                total += part;
+                part = 0;
+                label1.Text = "";
+
+            }
+        }
+        //=
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if (label1.Text.Length>0)
+            {
+                int a = Convert.ToInt32(Convert.ToString(label1.Text[0]));
+                int b = Convert.ToInt32(Convert.ToString(label1.Text[2]));
+                for (int i = 0; i < label1.Text.Length; i++)
+                {
+                    if (label1.Text[i] == '*')
+                    {
+                        part = a * b;
+                    }
+                    else if (label1.Text[i] == '/')
+                    {
+                        part = a / b;
+                    }
+                }
+                total += part;
+                part = 0;
+
+            }
+            label1.Text = Convert.ToString(total);
+        }
+        //C
+        private void button16_Click(object sender, EventArgs e)
+        {
+            total = 0;
+            part = 0;
+            oper = '0';
+            label1.Text = "";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (label1.Text.Length>0)
+            {
+                oper = '-';
+                int a = Convert.ToInt32(Convert.ToString(label1.Text[0]));
+                int b = Convert.ToInt32(Convert.ToString(label1.Text[2]));
+                if (label1.Text[1]=='*')
+                {
+                    part = a * b;
+                }
+                else
+                {
+                    part = a / b;
+                }
+            }
+            total += part;
+            part = 0;
+            label1.Text = "";
         }
     }
 }
